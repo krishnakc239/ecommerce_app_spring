4e131af8d9ceb2d1aba946478e39643a80c798a0
krishnakc239 <krishnakc2339@gmail.com>
2019-10-14 13:27:54 -0500

    /img resources ignored from security config

--- src/main/java/com/edu/mum/config/SecurityConfiguration.java
+++ src/main/java/com/edu/mum/config/SecurityConfiguration.java
@@ -1,13 +1,18 @@
 package com.edu.mum.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -16,25 +21,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-//    @Autowired
-//    private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-//    @Value("${spring.queries.users-query}")
-//    private String usersQuery;
-//
-//    @Value("${spring.queries.roles-query}")
-//    private String rolesQuery;
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth)
-//            throws Exception {
-//        auth.
-//                jdbcAuthentication()
-//                .usersByUsernameQuery(usersQuery)
-//                .authoritiesByUsernameQuery(rolesQuery)
-//                .dataSource(dataSource)
-//                .passwordEncoder(bCryptPasswordEncoder);
-//    }
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.
+                jdbcAuthentication()
+                .usersByUsernameQuery(usersQuery)
+                .authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,14 +47,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
 
         http.authorizeRequests()
-                .antMatchers("/", "/home", "/index").permitAll();
+                .antMatchers("/","/login","/signup", "/home", "/index").permitAll().anyRequest().permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login?error=true").defaultSuccessUrl("/")
+                .usernameParameter("userName").passwordParameter("password").permitAll()
+//                .loginProcessingUrl("/users/login")
+//                .successForwardUrl("/index")
+//                .failureForwardUrl("/error")
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").and().exceptionHandling()
+//                .accessDeniedPage("/error/403")
+        ;
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/login","/signup","/resources/**", "/uploads/**", "/templates/**", "/static/**", "/css/**", "/js/**", "/images/**");
+                .antMatchers("/resources/**","/uploads/**", "/templates/**", "/static/**", "/css/**", "/js/**", "/img/**");
     }
 
 
