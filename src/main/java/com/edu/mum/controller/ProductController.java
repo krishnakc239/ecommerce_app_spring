9693d096cf5e60c69268ea3c14baa0c83e39018f
krishnakc239 <krishnakc2339@gmail.com>
2019-10-16 23:02:12 -0500

    product add/edit/update issue fixex

--- src/main/java/com/edu/mum/controller/ProductController.java
+++ src/main/java/com/edu/mum/controller/ProductController.java
@@ -30,7 +30,7 @@ import java.util.Optional;
 @Controller
 public class ProductController {
 
-    private final static String upload_dir= System.getProperty("user.dir")+"/src/main/resources/static/uploads/images";
+    private final static String upload_dir = System.getProperty("user.dir") + "/src/main/resources/static/uploads/images";
 
     @Autowired
     private ProductService productService;
@@ -41,126 +41,108 @@ public class ProductController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/create")
-    public String create(@ModelAttribute Product product, Model model){
-        model.addAttribute("categories",categoryService.getAllCategory());
+    @GetMapping("/product/create")
+    public String create(@ModelAttribute Product product, Model model) {
+        model.addAttribute("categories", categoryService.getAllCategory());
         return "product/addProduct";
     }
 
-    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/product/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String index(@Valid @ModelAttribute Product product, @RequestParam("file") MultipartFile imgFile
-                                , BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) throws IOException {
-        if (bindingResult.hasErrors()){
-            model.addAttribute("successMessage", "There is problem creating this product");
+            , BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) throws IOException {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("successMessage", "There is problem in product form");
+            model.addAttribute("categories", categoryService.getAllCategory());
             return "product/addProduct";
-        }else {
+        } else {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             Optional<User> user = this.userService.findByUsername(auth.getName());
-            if( !user.isPresent() ){
+            if (!user.isPresent()) {
+                model.addAttribute("categories", categoryService.getAllCategory());
                 bindingResult.rejectValue("user", "error.product", "seller cannot be null");
+            } else {
+                product.setUser(user.get());
             }
             List<Product> savedProduct = productService.findAllByCode(product.getCode());
-            if (savedProduct.size()> 1){
+            if (savedProduct.size() > 1) {
                 product.updateQuantity();
             }
-            product.setCoverImage(imgFile.getOriginalFilename());
-            Path fileNameAndPath = Paths.get(upload_dir,imgFile.getOriginalFilename());
-
-            Files.write(fileNameAndPath,imgFile.getBytes());
-
-            product.setUser(user.get());
+            if (!imgFile.getOriginalFilename().isEmpty()) {
+                Path fileNameAndPath = Paths.get(upload_dir, imgFile.getOriginalFilename());
+                Files.write(fileNameAndPath, imgFile.getBytes());
+                product.setCoverImage(imgFile.getOriginalFilename());
+            }
             this.productService.create(product);
-            redirectAttributes.addFlashAttribute("successMessage","Product has been added");
+            redirectAttributes.addFlashAttribute("successMessage", "Product has been added");
 
-        }  return "redirect:/productList";
+        }
+        return "redirect:/productList";
     }
 
     @GetMapping("/productList")
-    public String index(@RequestParam(defaultValue = "0") int page, Model model){
+    public String index(@RequestParam(defaultValue = "0") int page, Model model) {
         Page<Product> products = this.productService.findAllProducts(page);
         Pager pager = new Pager(products);
         model.addAttribute("pager", pager);
         return "product/productList";
     }
 
-    @RequestMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id){
-        System.out.println("product to be deleted ="+id);
+    @RequestMapping("/product/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        System.out.println("product to be deleted =" + id);
         Optional<Product> product = this.productService.findById(id);
-//        if( !product.isPresent()){
-//            notifyService.addErrorMessage("Cannot find product #" + id);
-//        } else {
-//            productService.deleteById(id);
-            productService.delete(product.get());
-            System.out.println("product delted");
-//        }
+        productService.delete(product.get());
         return "redirect:/productList";
     }
+
     /**
      * Display for to edit a product
+     *
      * @param id
      * @param model
      * @return
      */
-    @GetMapping( "/edit/{id}" )
-    public String edit(@PathVariable("id") Long id, Model model){
+    @GetMapping("/product/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
         Optional<Product> product = productService.findById(id);
-        if( !product.isPresent()  ){
-            model.addAttribute("message","product not found");
+        if (!product.isPresent()) {
+            model.addAttribute("message", "product not found");
             return "redirect:product/productList";
         }
         Product productToEdit = product.get();
-        model.addAttribute("categories",categoryService.getAllCategory());
+        model.addAttribute("categories", categoryService.getAllCategory());
 //        System.out.println("pst to be edited :"+ product1.getUser().getId());
         model.addAttribute("product", productToEdit);
         return "product/editProduct";
     }
 
-    @PostMapping(value = "/edit", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/product/edit", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ModelAndView edit(@Valid @ModelAttribute Product product, BindingResult bindingResult,
-                            @RequestParam("file") MultipartFile imgFile) throws IOException {
+                             @RequestParam("file") MultipartFile imgFile) throws IOException {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("views/products/edit");
+        modelAndView.setViewName("/product/editProduct");
         // Perform validation
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             modelAndView.setViewName("product/editProduct");
             return modelAndView;
         }
-        User user = this.userService.findById(product.getUser().getId());
-        if( user==null ){
-            bindingResult.rejectValue("user", "error.product", "Author cannot be null");
-        }
 
-        if (imgFile!=null){
-            System.out.println("new image goging to be updated!!");
+        if (!imgFile.getOriginalFilename().isEmpty()) {
             product.setCoverImage(imgFile.getOriginalFilename());
-            Path fileNameAndPath = Paths.get(upload_dir,imgFile.getOriginalFilename());
-
-            Files.write(fileNameAndPath,imgFile.getBytes());
+            Path fileNameAndPath = Paths.get(upload_dir, imgFile.getOriginalFilename());
+            Files.write(fileNameAndPath, imgFile.getBytes());
         }
-//        if( !bindingResult.hasErrors() ){
-//            Optional<Product> productOptional = productService.findById(product.getId());
-//            if (productOptional.isPresent()){
-//                Product p = productOptional.get();
-//                p.setUser(user);
-//                p.setTitle(product.getTitle());
-//                p.setCategory(product.getCategory());
-//                p.setBody(product.getBody());
-                this.productService.create(product);
-                modelAndView.addObject("successMessage", "Product has been updated");
-                modelAndView.addObject("product", product);
-//            }
-//            product.setUser(user);
-
-//        }
+        this.productService.create(product);
+        modelAndView.addObject("successMessage", "Product has been updated");
+        modelAndView.addObject("product", product);
         return modelAndView;
     }
-    
+
 
     @GetMapping("/searchProduct")
     public String searchPost(@RequestParam(defaultValue = "0") int page,
-                             @RequestParam("searchParameter") String searchParameter, Model model){
-        Page<Product> products = productService.findAllByNameContainingIgnoreCaseOrCategory_CategoryNameContainingIgnoreCase(searchParameter,searchParameter,page);
+                             @RequestParam("searchParameter") String searchParameter, Model model) {
+        Page<Product> products = productService.findAllByNameContainingIgnoreCaseOrCategory_CategoryNameContainingIgnoreCase(searchParameter, searchParameter, page);
         Pager pager = new Pager(products);
         model.addAttribute("pager", pager);
 //        model.addAttribute("avgRatingMap", ArithmeticUtils.getAvgRatingMap(productService.findAll()));
