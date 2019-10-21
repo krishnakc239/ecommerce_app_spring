dddb26a1aa120d8723bc09ead4ad7b044a377089
Alican Ozer <alicanozer60@gmail.com>
2019-10-21 15:56:17 -0500

    shopping cart db and controller updated.

--- src/main/java/com/edu/mum/controller/CartController.java
+++ src/main/java/com/edu/mum/controller/CartController.java
@@ -1,7 +1,8 @@
 package com.edu.mum.controller;
 
-import com.edu.mum.domain.Cart;
+import com.edu.mum.domain.CartItem;
 import com.edu.mum.domain.Product;
+import com.edu.mum.service.CartItemService;
 import com.edu.mum.service.ProductService;
 import com.edu.mum.util.SessionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,34 +10,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
-@SessionAttributes("cartList")
+//@SessionAttributes("cartList")
 public class CartController {
 
-    List<Cart> cartList = new ArrayList<>();
     @Autowired
     private SessionUtils sessionUtils;
+
+    @Autowired
+    private CartItemService cartItemService;
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/addToCart/{pid}")
-    public String addToCart(@ModelAttribute Cart cart,
+    public String addToCart(
                             @PathVariable Integer pid,
                             @RequestParam(defaultValue = "1") String orderQuantity,
                             Model model){
         Optional<Product> product = productService.findById(Long.valueOf(pid));
         Product addedProduct = product.isPresent()? product.get(): new Product();
         model.addAttribute("product",addedProduct);
-        cart.setProduct(addedProduct);
-        cart.setQuantity(Integer.parseInt(orderQuantity));
-        cart.setStock(addedProduct.getQuantity());
-        cartList.add(cart);
-        model.addAttribute("cartList",cartList);
+        CartItem cartItem = new CartItem();
+        cartItem.setProduct(addedProduct);
+        cartItem.setQuantity(Integer.parseInt(orderQuantity));
+        cartItem.setStock(addedProduct.getQuantity());
+        cartItem.setUser(sessionUtils.getCurrentUser());
+
+        cartItemService.save(cartItem);
+        model.addAttribute("cartList", cartItemService.findAll());
         return "product/cart";
     }
 
