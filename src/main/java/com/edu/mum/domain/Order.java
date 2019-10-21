ce73078f267eddd074e4385ba2c92c3a5e42646d
krishnakc239 <krishnakc2339@gmail.com>
2019-10-21 16:21:29 -0500

    Address and orders domain serive/iml repository added

---
+++ src/main/java/com/edu/mum/domain/Order.java
@@ -0,0 +1,114 @@
+package com.edu.mum.domain;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date orderDate;
+
+    private int orderNum;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToMany
+    @JoinColumn(name = "cart_id")
+    private List<CartItem> cartItemList;
+
+    private Address billingAddress;
+    private Address shippingAddress;
+
+    private String status;
+
+    private double totalAount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(int orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<CartItem> getCartItemList() {
+        return cartItemList;
+    }
+
+    public void setCartItemList(List<CartItem> cartItemList) {
+        this.cartItemList = cartItemList;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getTotalAount() {
+        double sum =0.00;
+        for (CartItem c:this.cartItemList) {
+            sum = c.getAmount();
+        }
+        return sum;
+    }
+
+    public void setTotalAount(double totalAount) {
+        this.totalAount = totalAount;
+    }
+}
---
+++ src/main/java/com/edu/mum/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.edu.mum.repository;
+
+import com.edu.mum.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
