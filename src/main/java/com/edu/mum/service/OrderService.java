d899140b85c53dbd03e989c153524295daeaaa1a
zayed016 <zayedemails@gmail.com>
2019-10-21 22:01:56 -0500

    orderlist ok

--- src/main/java/com/edu/mum/service/OrderService.java
+++ src/main/java/com/edu/mum/service/OrderService.java
@@ -1,4 +1,9 @@
 package com.edu.mum.service;
 
+import com.edu.mum.domain.Order;
+import com.edu.mum.domain.User;
+import org.springframework.data.domain.Page;
+
 public interface OrderService {
+    Page<Order> findAllByUser(User user, int page);
 }
