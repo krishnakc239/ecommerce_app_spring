856cb1fa40f4a5bae9184a91eb821e1c5b086aa2
krishnakc239 <krishnakc2339@gmail.com>
2019-10-22 12:09:16 -0500

    raw credit card info data added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,3 +26,8 @@ values
         (null,'Kitchen'),
         (null,'Clothes');
 
+insert into credit values(null, 20000, 1234567891234567, 'c02', '12/24');
+insert into credit values(null, 20000, 2234567891234567, 'c02', '12/24');
+insert into credit values(null, 20000, 3234567891234567, 'c02', '12/24');
+
+
